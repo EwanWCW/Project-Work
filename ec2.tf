71960f60802861ca423ba1f3c3882a0dfadabb2a
EwanWCW <ewan_wait@hotmail.com>
2024-10-02 12:17:29 +0100

    123

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+
+resource "aws_instance" "Natwest-Vm-Ewan" {
+  count                  = 3
+  ami                    = "ami-08978028fd061067a"
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.nkpEW.key_name
+  vpc_security_group_ids = [aws_security_group.sg1.id]
+  subnet_id              = aws_subnet.sn1.id
+  tags = {
+    "Name" = "Natwest-Vm-Ewan-Ansible-${count.index}"
+  }
+  associate_public_ip_address = true
+}
+
+output "PublicIpAddress" {
+   value = aws_instance.Natwest-Vm-Ewan[0].public_ip
+}
---
+++ securitygroup.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "sg1" {
+  name   = "Ewan-trainer-sg1"
+  vpc_id = aws_vpc.vpc1EW.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
