a0bc7766e82ee3edeca5b769e0ef415b007ef6d5
EwanWCW <ewan_wait@hotmail.com>
2024-10-02 12:15:31 +0100

    Remove .terraform directory from tracking

--- ig.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "gw1" {
-  vpc_id = aws_vpc.vpc1EW.id
-  tags = {
-    Name = "IG-Natwest"
-  }
-}
--- publickeys.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "nkpEW" {
-  key_name   = "Ewan-key-pair"
-  public_key = var.publickey
-}
